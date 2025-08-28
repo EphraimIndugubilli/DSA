3157bc637f5724d04479c1c795c652ef140cfd72
Ephraim Indugubilli <ephraimindugubilli@gmail.com>
2025-08-28 22:52:21 +0530

    MergeSort

---
+++ MergeSort.java
@@ -0,0 +1,54 @@
+//time complexity: O(nlogn)
+//space complexity: O(n)  
+import java.util.*;
+class MergeSort{
+    public static void MergeSort(ArrayList<Integer> arr, int low, int high){
+        if(low >= high) return;
+        int mid = (low + high) / 2;
+        MergeSort(arr, low, mid);
+        MergeSort(arr, mid + 1, high);
+        Merge(arr, low, mid, high);
+    }
+
+    public static void Merge(ArrayList<Integer> arr, int l, int m, int h){
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = l;
+        int right = m + 1;
+
+        // merging the elements;
+        while(left <= m && right <= h){
+            if(arr.get(left) < arr.get(right)){
+                temp.add(arr.get(left));
+                left++;
+            } else {
+                temp.add(arr.get(right));
+                right++;
+            }
+        }
+        // pushing left over elements to array, if there exist elements;
+        while(left <= m){
+            temp.add(arr.get(left));
+            left++;
+        }
+        while(right <= h){
+            temp.add(arr.get(right));
+            right++;
+        }
+        for(int i = l; i <= h; i++){
+            arr.set(i, temp.get(i - l));
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            arrayList.add(sc.nextInt());
+        }
+        MergeSort(arrayList, 0, n - 1);
+        for(int i = 0; i < n; i++){
+            System.out.print(arrayList.get(i) + " ");
+        }
+    }
+}
