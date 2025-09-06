cdcbc5eaf03bb53cad832a5c23d2b9543d6052cc
Ephraim Indugubilli <ephraimindugubilli@gmail.com>
2025-09-06 13:44:25 +0530

    rotation array code

---
+++ ArrayRotation.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class ArrayRotation{
+    public static void rotateArr(int[] arr,int n){
+        int temp = arr[0];
+        for(int i =0;i<=n-1;i++){
+            if(i<n-1){
+                for(int j = i+1;j<=n-1;j++){
+                    arr[i] = arr[i+1];
+                }
+            }else{
+                arr[i] = temp;
+            }
+            
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0;i<=n-1;i++){
+            arr[i] = sc.nextInt();
+        }
+        rotateArr(arr,n);
+        System.out.println("Array after rotation: ");
+        for(int i = 0;i<=n-1;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
